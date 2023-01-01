4f359de2ddca0f358fd0c2c839a7583069bcc5b0
Lívia Nascimento <liviapnd@gmail.com>
2023-01-01 19:43:03 -0300

    beecrowd 1002

---
+++ beecrowd/src/iniciante/AreaDoCirculo1002.java
@@ -0,0 +1,21 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class AreaDoCirculo1002 {
+	
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+
+		double raio = ler.nextDouble();
+
+		ler.close();
+
+		double area = raio * raio * 3.14159;
+
+		System.out.println(String.format("A=%.4f", area));
+		
+	}
+	
+}
