2227986b6129b02034fd928072a9eeaf223deac6
Lívia Nascimento <liviapnd@gmail.com>
2023-01-02 13:38:00 -0300

    beecrowd 1035

--- beecrowd/src/iniciante/MediaSimples1005.java
+++ beecrowd/src/iniciante/MediaSimples1005.java
@@ -1,5 +1,22 @@
 package iniciante;
 
+import java.util.Scanner;
+
 public class MediaSimples1005 {
 
+	public static void main(String[] args) {
+
+		Scanner ler = new Scanner(System.in);
+
+		double A = ler.nextDouble();
+		double B = ler.nextDouble();
+
+		ler.close();
+
+		double MEDIA = ((A * 3.5) + (B * 7.5)) / (3.5 + 7.5);
+
+		System.out.printf("MEDIA = %.5f\n", MEDIA);
+	
+	}
+	
 }
