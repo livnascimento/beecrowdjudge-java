482be34085435cc6ec34a6b8999896789a02be5a
Lívia Nascimento <liviapnd@gmail.com>
2023-01-03 14:16:26 -0300

    beecrowd 1041

---
+++ beecrowd/src/iniciante/Coordenadas1041.java
@@ -0,0 +1,34 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Coordenadas1041 {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		float x = leitor.nextFloat();
+		float y = leitor.nextFloat();
+		
+		if (x == 0 && y == 0) {
+			System.out.println("Origem");
+		} else if (x == 0) {
+			System.out.println("Eixo Y");
+		} else if (y == 0) {
+			System.out.println("Eixo X");
+		} else if (x > 0 && y > 0) {
+			System.out.println("Q1");
+		} else if (x > 0 && y < 0) {
+			System.out.println("Q4");
+		} else if (x < 0 && y > 0) {
+			System.out.println("Q2");
+		} else if (x < 0 && y < 0) {
+			System.out.println("Q3");
+		}
+		
+		leitor.close();
+		
+	}
+	
+}
