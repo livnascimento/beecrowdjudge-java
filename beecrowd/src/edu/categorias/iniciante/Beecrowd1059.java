86a4225c39cfcbdc06ec04b13cdb45333790034d
Lívia Nascimento <liviapnd@gmail.com>
2023-01-09 12:15:23 -0300

    Create beecrowd 1059

---
+++ beecrowd/src/edu/categorias/iniciante/Beecrowd1059.java
@@ -0,0 +1,13 @@
+package edu.categorias.iniciante;
+
+public class Beecrowd1059 {
+	
+	public static void main(String[] args) {
+		
+		for (int i  = 2; i <= 100 ; i += 2) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
