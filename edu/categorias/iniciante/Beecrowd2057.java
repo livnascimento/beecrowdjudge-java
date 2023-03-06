60bc66fb63bd76ba9d7c5bd9cdaaa1bf0468b8ee
Lívia Nascimento <liviapnd@gmail.com>
2023-03-06 14:57:16 -0300

    Fuso horário

---
+++ edu/categorias/iniciante/Beecrowd2057.java
@@ -0,0 +1,28 @@
+package edu.categorias.iniciante;
+
+import java.util.Scanner;
+
+public class Beecrowd2057 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int saida = sc.nextInt();
+		int tempoViagem = sc.nextInt();
+		int fuso = sc.nextInt();
+		
+		int horarioChegada = saida + tempoViagem + fuso;
+		
+		System.out.println(horarioChegada);
+		
+		if (horarioChegada >= 24) horarioChegada -= 24;
+		else if (horarioChegada < 0) horarioChegada += 24;
+
+		System.out.println(horarioChegada);
+		
+		sc.close();
+		
+	}
+
+}
