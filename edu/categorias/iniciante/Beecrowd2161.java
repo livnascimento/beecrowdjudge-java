ed10090ac3cccf0d791b8586fd65b6d580e5cff7
Lívia Nascimento <liviapnd@gmail.com>
2023-03-31 12:03:10 -0300

    Raiz quadrada de 10

--- edu/categorias/iniciante/Beecrowd2161.java
+++ edu/categorias/iniciante/Beecrowd2161.java
@@ -3,28 +3,24 @@ package edu.categorias.iniciante;
 import java.util.Scanner;
 
 public class Beecrowd2161 {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int contador = 3;
-		double resultado;
-				
-		if (n == 0)	resultado = contador;
-		else {
-			resultado = contador;
-			contador *= 2;
-			for (int i = 0 ; i < n ; i++) {
-				resultado += 1/contador;
-			}
+		double resultado = 0;
+
+		for (int i = 0; i < n; i++) {
+			resultado = 1.0 / (resultado + 6.0);
 		}
-		
+		resultado += 3.0;
+
 		System.out.printf("%.10f\n", resultado);
-		
+
 		sc.close();
-		
+
 	}
 
 }
--- module-info.java
+++ module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module beecrowd {
+	requires java.scripting;
 }
